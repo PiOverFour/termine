53ebadda28ba1c197bb894702ea0d9313c32995e
Damien Picard <dam.pic@free.fr>
2017-01-05 01:18:26 +0100

    Grid generation

--- termine.py
+++ termine.py
@@ -5,15 +5,16 @@ from __future__ import print_function
 import argparse
 import re
 import sys
+import random
 
 ROWS = 'abcdefghjklmnopqrstuvwxyz'
 
 parser = argparse.ArgumentParser(description='A terminal minesweeper clone')
 parser.add_argument('-gs', '--gridsize', metavar='WxH',
-                    type=str, default='30x16',
+                    type=str, default='8x8',
                     help='Size of the grid')
 parser.add_argument('-m', '--mines', metavar='N',
-                    type=int, default=99,
+                    type=int, default=10,
                     help='Number of mines')
 args = parser.parse_args()
 
@@ -28,28 +29,77 @@ except:
 # print(width, height, type(height))
 if mines > width * height:
     print('    Error: Number of mines too high')
-    # parser.print_help()
     sys.exit(1)
 if mines < 1:
     print('    Error: Number of mines too low')
-    # parser.print_help()
     sys.exit(1)
 
 
-def print_grid():
+class Cell(object):
+    def __init__(self, coords, grid, is_mine):
+        self.grid = grid
+        self.coords = coords
+        self.is_mine = is_mine
+
+    def compute(self):
+        self.adjacent = random.randint(0, 8)
+
+    def open(self):
+        pass
+
+
+def generate_grid(width, height, mines):
+    grid = []
+    mines = [True if (n < mines) else False for n in range(width*height)]
+    random.shuffle(mines)
+    for x in range(width):
+        row = []
+        for y in range(height):
+            row.append(Cell((x, y), grid, mines[y*width+x]))
+        grid.append(row)
+    print(grid)
+    # grid = [[Cell((x, y)) for y in range(height)] for x in range(width)]
+
+    # grid = [[random.randint(0, 8) for y in range(height)] for x in range(width)]
+    return grid
+
+
+def compute_grid(grid):
+    for x in range(width):
+        for y in range(height):
+            grid[x][y].compute()
+
+
+def print_grid(grid):
     # header
     print('   ', end='')
+    # draw horizontal coordinates
     for x in range(width):
         print('% 4i' % (x+1), end='')
+
     print('\n    ╔═══' + '╤═══' * (width-1) + '╗')
     for y in range(height):
-        # for x in range(width):
         if y > 0:
             print('    ╟───' + '┼───' * (width-1) + '╢')
-        print(' ' + ROWS[y] + '  ║   ' + '│   ' * (width-1) + '║')
+        # draw vertical coordinates
+        print(' ' + ROWS[y] + '  ║', end='')
+        for x in range(width):
+            if x:
+                print('│', end='')
+            is_mine = 'X' if grid[x][y].is_mine else ' '
+            print(' %s ' % is_mine, end='')
+        print('║')
         # print('')
     print('    ╚═══' + '╧═══' * (width-1) + '╝')
 
-    fdsf = raw_input('\n    Please enter the next move: ')
 
-print_grid()
+game_ended = False
+
+if __name__ == '__main__':
+    grid = generate_grid(width, height, mines)
+    compute_grid(grid)
+    print(grid)
+    while not game_ended:
+        print_grid(grid)
+        next_move = raw_input('\n    Please enter the next move: ')
+        compute_grid(next_move)
